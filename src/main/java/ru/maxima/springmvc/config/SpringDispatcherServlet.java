56a54bb828f44697eaae0a769e33ea0ff22341ce
yatsenko.v <FDT3sRtwD>
2023-06-15 19:35:29 +0300

    commit

---
+++ src/main/java/ru/maxima/springmvc/config/SpringDispatcherServlet.java
@@ -0,0 +1,20 @@
+package ru.maxima.springmvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringDispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
--- src/main/java/ru/maxima/springmvc/controllers/FirstController.java
+++ src/main/java/ru/maxima/springmvc/controllers/FirstController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FirstController {
 
-    // localhost:8080/hello
+    // Аннотация служит для того, чтобы рутировать запрос
     @GetMapping("/hello")
     public String hello() {
+        // вернет нам представление, которое наодится в папке webapp/WEB-INF/views/
         return "hello_world";
     }
 }
