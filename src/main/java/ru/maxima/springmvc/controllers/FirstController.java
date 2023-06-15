8a14b9ef097e89c0fe3e943daf9e45cfcf757e9a
yatsenko.v <FDT3sRtwD>
2023-06-15 19:02:32 +0300

    commit

--- src/main/java/ru/maxima/springmvc/controllers/FirstController.java
+++ src/main/java/ru/maxima/springmvc/controllers/FirstController.java
@@ -1,2 +1,14 @@
-package ru.maxima.springmvc.controllers;public class FirstController {
+package ru.maxima.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    // localhost:8080/hello
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello_world";
+    }
 }
